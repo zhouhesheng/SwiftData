e93ab8eb3b778dd1e894fed9ffc74ff5fcde2654
Zhou Hesheng <hzhou@ilegendsoft.com>
2018-11-16 14:41:38 +0800

    Swift 4

---
+++ SwiftData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftData
+//
+//  Created by 周和生 on 2018/11/16.
+//  Copyright © 2018 LinkedIn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
